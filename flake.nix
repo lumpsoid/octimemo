2a3443756d7ba4c53beccf8b3a49baf32726155c
lumpsoid <mmgeeki@gmail.com>
2024-12-28 12:15:19 +0100

    chore: bump deps

--- flake.nix
+++ flake.nix
@@ -18,120 +18,134 @@
       inputs.flake-utils.follows = "flake-utils";
       inputs.devshell.follows = "devshell";
     };
-    
   };
 
-  outputs = { self, nixpkgs, flake-utils, android-nixpkgs, ... }:
-  flake-utils.lib.eachDefaultSystem (system: 
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        android_sdk.accept_license = true;
-        allowUnfree = true;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    android-nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
       };
-    };
-    # Everything to make Flutter happy
-    sdk = android-nixpkgs.sdk.${system} (sdkPkgs:
-      with sdkPkgs; [
-        cmdline-tools-latest
-        build-tools-30-0-3
-        build-tools-34-0-0
-        platform-tools
-        emulator
-        #patcher-v4
-        platforms-android-34
-      ]);
-    pinnedJDK = pkgs.jdk17;
-    pinnedFlutter = pkgs.flutter;
-  in 
-  {
-    # don't need to write the <system> part
-    # because we inherited system in pkgs
-    devShells = {
-      default = pkgs.mkShell {
-        name = "octimemo-devshell";
+      # Everything to make Flutter happy
+      sdk = android-nixpkgs.sdk.${system} (sdkPkgs:
+        with sdkPkgs; [
+          cmdline-tools-latest
+          build-tools-30-0-3
+          build-tools-34-0-0
+          platform-tools
+          emulator
+          #patcher-v4
+          platforms-android-34
+        ]);
+      pinnedJDK = pkgs.jdk17;
+      pinnedFlutter = pkgs.flutter;
+      shellSh =
+        /*
+        sh
+        */
+        ''
+          export PATH="$PATH":"$HOME/.pub-cache/bin"
 
-        buildInputs = [
-          # Android
-          pinnedJDK
-          sdk
+          session="octimemo-flutter"
+          sessionExist=$(tmux list-sessions | grep $session)
 
-          # Flutter
-          pinnedFlutter
-
-          # Code hygiene
-          pkgs.gitlint
-        ];
-
-        # android specific envs
-        ANDROID_HOME = "${sdk}/share/android-sdk";
-        ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
-        JAVA_HOME = pinnedJDK;
-
-        GRADLE_USER_HOME = "/home/qq/.gradle";
-        # Fix an issue with Flutter using an older version of aapt2, which does not know
-        # an used parameter.
-        #GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
-
-        shellHook = ''
-          ./tmux-script.sh
-        '';
-      };
-      pc = {
-        buildInputs = with pkgs; [
-          # Flutter
-          flutter
-
-          # Code hygiene
-          gitlint
-
-          # Flutter dependencies for linux desktop
-          atk
-          cairo
-          clang
-          cmake
-          epoxy
-          gdk-pixbuf
-          glib
-          gtk3
-          harfbuzz
-          ninja
-          pango
-          pcre
-          pkg-config
-          xorg.libX11
-          xorg.xorgproto
-        ];
-
-        # Make Flutter build on desktop
-        CPATH = "${pkgs.xorg.libX11.dev}/include:${pkgs.xorg.xorgproto}/include";
-        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [atk cairo epoxy gdk-pixbuf glib gtk3 harfbuzz pango];
-
-        shellHook = ''
-          #!/bin/env sh
-
-          session="octimemo"
-          sessioExist=$(tmux list-sessions | grep $session)
-
-          if [ "$sessioExist" != "" ]; then
+          if [ "$sessionExist" != "" ]; then
               tmux kill-session -t $session
           fi
           window=0
 
-          tmux -L "$session" # make it able to inherit the shell variable
+          tmux -L $session new-session -d -s $session
 
-          tmux new-session -d -s $session
-          tmux set -g mouse on
-          tmux set -g mouse-select-window on
+          # Set tmux options
+          tmux -L $session set -g mouse on
+          tmux -L $session set -g mouse-select-window on
 
-          tmux split-window -v -t $session:$window.0
+          # Create the first window and run nvim
+          tmux -L $session send-keys -t $session 'nvim ./lib' C-m
 
-          tmux attach-session -t $session
+          # Split the window and run flutter
+          tmux -L $session split-window -v -t $session:0.0
+          tmux -L $session send-keys -t $session 'flutter run --flavor development --target ./lib/main_development.dart --debug' C-m
+
+          # Select the first pane
+          tmux -L $session select-pane -t 0
+
+          # Attach to the session
+          tmux -L $session attach-session -t $session
+
         '';
+    in {
+      # don't need to write the <system> part
+      # because we inherited system in pkgs
+      devShells = {
+        default = pkgs.mkShell {
+          name = "octimemo-devshell";
+
+          buildInputs = [
+            # Android
+            pinnedJDK
+            sdk
+
+            # Flutter
+            pinnedFlutter
+
+            # Code hygiene
+            pkgs.gitlint
+          ];
+
+          # android specific envs
+          ANDROID_HOME = "${sdk}/share/android-sdk";
+          ANDROID_SDK_ROOT = "${sdk}/share/android-sdk";
+          JAVA_HOME = pinnedJDK;
+
+          GRADLE_USER_HOME = "/home/qq/.gradle";
+          # Fix an issue with Flutter using an older version of aapt2, which does not know
+          # an used parameter.
+          #GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+
+          shellHook = shellSh;
+        };
+        pc = {
+          buildInputs = with pkgs; [
+            # Flutter
+            flutter
+
+            # Code hygiene
+            gitlint
+
+            # Flutter dependencies for linux desktop
+            atk
+            cairo
+            clang
+            cmake
+            epoxy
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            ninja
+            pango
+            pcre
+            pkg-config
+            xorg.libX11
+            xorg.xorgproto
+          ];
+
+          # Make Flutter build on desktop
+          CPATH = "${pkgs.xorg.libX11.dev}/include:${pkgs.xorg.xorgproto}/include";
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [atk cairo epoxy gdk-pixbuf glib gtk3 harfbuzz pango];
+
+          shellHook = shellSh;
+        };
       };
-    };
-  });
+    });
 }
-
-
